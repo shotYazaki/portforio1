764fc618f8c544a0094cc80c841c94010637ed34
Shota Yamazaki <yatohs0601@gmail.com>
2021-01-19 17:56:03 +0900

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactYoutube from '../ReactYoutube';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <ReactYoutube videoId = 'Igz41-Voa58' />
+        )
+    }
+}
+
+export default Home
