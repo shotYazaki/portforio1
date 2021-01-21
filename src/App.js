b9f64938268cae4a2c82c0bbad3f17fa550b0117
Shota Yamazaki <yatohs0601@gmail.com>
2021-01-21 16:42:29 +0900

    Edit App.js add <Home />

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {  Component }from 'react'
+import React, {  Component }from 'react';
+import Home from './Home';
 
-import ReactYoutube from './ReactYoutube'
 
 import './App.css';
 
@@ -8,9 +8,7 @@ class App extends Component {
   render() {
     return(
       <div className='App'>
-        <ReactYoutube videoId = 'AOuo4q0xFNc' />
-        <ReactYoutube videoId = 'LY6HHTx33JM' />
-        <ReactYoutube videoId = '9GvTYd5hG_g' />
+        <Home />
       </div>
     )
   }
