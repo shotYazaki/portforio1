e2ab8c33dea7617e5dd94008bfbfc7b79230328f
Shota Yamazaki <yatohs0601@gmail.com>
2021-01-27 10:48:38 +0900

    Add Header by React-bootstrap Fixes #3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import'./App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './component/Home';
-
+import Header from './component/Header';
 
 import './App.css';
 
 function App () {
     return(
       <div className='App'>
+        <Header />
         <Home />
       </div>
     );
