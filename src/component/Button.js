800c23bf7475cdce04d07ad2a606891e0e91c6b6
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-05 15:25:18 +0900

    edit Button.js YoutubePlay

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-
-export default class Button extends React.Component {
+export default class PlayButton extends React.Component {
     render() {
         return(
             <div>
-                
+              <Button variant="primary" size ="lg" active>
+                 Youtube play
+              </Button>
             </div>
         )
     }
