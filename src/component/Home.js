e5e0afb364fdbbcf48093a9492ebec768a2882f4
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-05 14:46:34 +0900

    Edit Home.js file Add rendere() {}

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,15 +3,17 @@ import ReactYoutube from '../ReactYoutube';
 import Button from './Button';
 
 
-function Home () {
-    return (
-        <div className='Youtube-video'>
-            <Button />
-            <ReactYoutube videoId = 'vlDzYIIOYmM' />
-            <ReactYoutube videoId = 'Ks71YWIDEuM' />
-            <ReactYoutube videoId = 'Jqf9haCd6mM' />
-        </div>
-    );
+class Home extends React.Component() {
+    render() {
+        return(
+            <div className='Youtube-video'>
+               <Button />
+               <ReactYoutube videoId = 'vlDzYIIOYmM' />
+               <ReactYoutube videoId = 'Ks71YWIDEuM' />
+               <ReactYoutube videoId = 'Jqf9haCd6mM' />
+            </div>
+        );
+    }
 }
 
 export default Home;
