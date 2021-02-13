74f6630a5cd9d36bb0162a33553f6f323185cffc
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-13 15:22:20 +0900

    deleted Button in Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactYoutube from '../ReactYoutube';
-import Button from './Button';
 
 
 class Home extends React.Component {
     render() {
         return(
             <div className='Youtube-video'>
-                <div className='Button-play'>
-                    <Button />
-                </div>
                <ReactYoutube videoId = 'vlDzYIIOYmM' />
                <ReactYoutube videoId = 'Ks71YWIDEuM' />
                <ReactYoutube videoId = 'Jqf9haCd6mM' />
